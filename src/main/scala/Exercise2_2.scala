67afc91b47294a358f0af4c1b4830b3275fb63e3
rurik <urrast@yandex.ru>
2020-10-23 00:48:29 +0400

    init commit

---
+++ src/main/scala/Exercise2_2.scala
@@ -0,0 +1,18 @@
+object Exercise2_2 {
+
+  def isSorted[A](arr: Array[A], ordered: (A, A) => Boolean): Boolean = {
+    @annotation.tailrec
+    def loop(n: Int): Boolean = {
+      if (n > arr.length - 2) {
+        true
+      } else if (ordered(arr(n), arr(n + 1))) {
+        loop(n + 1)
+      } else {
+        false
+      }
+    }
+
+    loop(0)
+  }
+
+}
---
+++ src/test/scala/Exercise2_2Spec.scala
@@ -0,0 +1,27 @@
+
+import Exercise2_2.isSorted
+import org.scalatest._
+import org.scalatest.flatspec._
+import org.scalatest.matchers._
+
+class Exercise2_2Spec extends AnyFlatSpec with should.Matchers {
+
+  private val intOrdered: (Int, Int) => Boolean = (i: Int, j: Int) => i < j
+
+  "Array" should "be sorted" in {
+
+    isSorted(Array(1, 2, 3, 4, 5), intOrdered) should be (true)
+
+    isSorted(Array(-1,0,1, 2, 3, 4, 5,100,2000), intOrdered) should be (true)
+
+  }
+
+  "Array" should "not be sorted" in {
+
+    isSorted(Array(3,2), intOrdered) should be (false)
+
+    isSorted(Array(5,4,3,55,2,1), intOrdered) should be (false)
+
+  }
+
+}
