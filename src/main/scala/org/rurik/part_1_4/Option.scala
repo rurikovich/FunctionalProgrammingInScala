f85eeb681cda03ac1ef8900cf86b165402e144ab
rurik <urrast@yandex.ru>
2020-10-28 19:32:51 +0400

    option

--- src/main/scala/org/rurik/part_1_4/Option.scala
+++ src/main/scala/org/rurik/part_1_4/Option.scala
@@ -27,6 +27,47 @@ sealed trait Option[+A] {
     case _ => None
   }
 
+
+}
+
+object Option {
+  def variance(xs: Seq[Double]): Option[Double] = {
+    val m = xs.sum / xs.size
+
+    def f(x: Double): Double = Math.pow(x - m, 2)
+
+    Some(xs.map(f).sum / xs.size)
+  }
+
+  def map2[A, B, C](aOpt: Option[A], bOpt: Option[B])(f: (A, B) => C): Option[C] = (aOpt, bOpt) match {
+    case (None, _) => None
+    case (_, None) => None
+    case (Some(a), Some(b)) => Some(f(a, b))
+  }
+
+  def sequence[A](a: List[Option[A]]): Option[List[A]] = {
+    val emptyList: Option[List[A]] = Some(List.empty[A])
+    a.foldLeft(emptyList) {
+      (listOpt: Option[List[A]], aOp: Option[A]) =>
+        listOpt.flatMap {
+          list => aOp.map(a => list ++ List(a))
+        }
+    }
+  }
+
+  def traverse[A, B](a: List[A])(f: A => Option[B]): Option[List[B]] = {
+    val emptyList: Option[List[B]] = Some(List.empty[B])
+
+    a.foldLeft(emptyList) {
+      (listOpt: Option[List[B]], aa: A) =>
+        for {
+          list <- listOpt
+          b <- f(aa)
+        } yield list ++ List(b)
+    }
+  }
+
+
 }
 
 case class Some[+A](get: A) extends Option[A]
--- src/test/scala/org/rurik/part_1_4/Exercise4_1Spec.scala
+++ src/test/scala/org/rurik/part_1_4/Exercise4_1Spec.scala
@@ -1,7 +1,10 @@
 package org.rurik.part_1_4
 
+import org.rurik.part_1_4.Option.{map2, sequence, traverse, variance}
 import org.scalatest.flatspec._
 import org.scalatest.matchers._
+
+import scala.util.Try
 
 class Exercise4_1Spec extends AnyFlatSpec with should.Matchers {
 
@@ -22,6 +25,33 @@ class Exercise4_1Spec extends AnyFlatSpec with should.Matchers {
 
     Some(1).filter(_ > 0) shouldBe Some(1)
     none.filter(_ > 0) shouldBe None
+
+  }
+
+  "variance" should "operates correctly" in {
+    variance(List(2, 4, 4, 4, 5, 5, 7, 9)) shouldBe Some(4.0)
+  }
+
+
+  "map2" should "operates correctly" in {
+    map2(None: Option[Int], None: Option[Int])(_ + _) shouldBe None
+    map2(Some(2), None: Option[Int])(_ + _) shouldBe None
+    map2(None: Option[Int], Some(2))(_ + _) shouldBe None
+    map2(Some(2), Some(2))(_ + _) shouldBe Some(4)
+  }
+
+  "sequence" should "operates correctly" in {
+    sequence(List(None)) shouldBe None
+    sequence(List(Some(1), Some(1), Some(2))) shouldBe Some(List(1, 1, 2))
+    sequence(List(Some(1), Some(1), Some(2), None)) shouldBe None
+  }
+
+  "traverse" should "operates correctly" in {
+    val f: Int => Option[String] = i => if (i % 2 !== 0) Some(s"$i") else None
+
+    traverse(List(2))(f) shouldBe None
+    traverse(List(1, 3, 4))(f) shouldBe None
+    traverse(List(1, 3, 5, 7))(f) shouldBe Some(List("1", "3", "5", "7"))
 
   }
 
