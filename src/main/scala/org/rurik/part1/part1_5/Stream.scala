089dafa973f52b0b220c258787d01aebe590751a
rurik <urrast@yandex.ru>
2020-10-31 17:08:06 +0400

    Stream  map, filter, append, and flatMap

--- src/main/scala/org/rurik/part1/part1_5/Stream.scala
+++ src/main/scala/org/rurik/part1/part1_5/Stream.scala
@@ -56,11 +56,15 @@ sealed trait Stream[+A] {
 
   def headOption: Option[A] = foldRight(None: Option[A])((h, _) => Some(h))
 
-//  def map[B](l: Stream[A])(f: A => B): Stream[B] = ???
-//
-//  def append(l: Stream[A], r: Stream[A]): Stream[A] = ???
+  def map[B](f: A => B): Stream[B] = foldRight(Stream[B]())((a, b) => cons[B](f(a), b))
 
+  def filter(f: A => Boolean): Stream[A] = foldRight(Stream[A]()) {
+    (a, b) => if (f(a)) cons[A](a, b) else b
+  }
 
+  def append[B >: A](s: => Stream[B]): Stream[B] = foldRight(s)((a, b) => cons(a, b))
+
+  def flatMap[B](f: A => Stream[B]): Stream[B] = foldRight(Stream[B]())((a, b) => f(a).append(b))
 }
 
 case object Empty extends Stream[Nothing]
--- src/test/scala/org/rurik/part1/part1_5/StreamSpec.scala
+++ src/test/scala/org/rurik/part1/part1_5/StreamSpec.scala
@@ -50,6 +50,23 @@ class StreamSpec extends AnyFlatSpec with should.Matchers {
     Stream[Int]().headOption shouldBe None
   }
 
+  "Stream" should "operate map correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).map(a => a * 2).toList shouldBe List(2, 4, 6, 8, 10)
+    Stream[Int]().map(a => a * 2).toList shouldBe List()
+  }
 
+  "Stream" should "operate filter correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).filter(_ < 3).toList shouldBe List(1, 2)
+    Stream[Int]().filter(_ < 3).toList shouldBe List()
+  }
+
+  "Stream" should "operate append correctly" in {
+    Stream[Int](1, 2).append(Stream[Int](3, 4, 5)).toList shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  "Stream" should "operate flatMap correctly" in {
+    Stream[Int](1, 2, 3).flatMap(a => Stream(a * 2)).toList shouldBe List(2, 4, 6)
+    Stream[Int]().flatMap(a => Stream(a * 2)).toList shouldBe List()
+  }
 
 }
