8021368c94ff4fb4716fd84bd3114fe49cb9b877
rurik <urrast@yandex.ru>
2020-10-31 16:49:41 +0400

    Stream

--- src/main/scala/org/rurik/part1/part1_5/Stream.scala
+++ src/main/scala/org/rurik/part1/part1_5/Stream.scala
@@ -1,15 +1,65 @@
 package org.rurik.part1.part1_5
 
+import org.rurik.part1.part1_5.Stream.cons
+
+import scala.annotation.tailrec
+
 sealed trait Stream[+A] {
 
-  def toList: List[A] = this match {
-    case Cons(h, tail) => h() :: tail().toList
-    case Empty => List.empty[A]
+  def toList: List[A] = {
+
+    @tailrec
+    def go(s: Stream[A], acc: List[A]): List[A] =
+      s match {
+        case Cons(h, tail) => go(tail(), h() :: acc)
+        case _ => acc
+      }
+
+    go(this, List()).reverse
   }
 
-  def take(n:Int):Stream[A]={
-
+  def take(n: Int): List[A] = (n, this) match {
+    case (0, _) => List.empty[A]
+    case (_, Empty) => List.empty[A]
+    case (_, Cons(h, tail)) => h() :: tail().take(n - 1)
   }
+
+  @tailrec
+  final def drop(n: Int): Stream[A] = (n, this) match {
+    case (0, t) => t
+    case (_, Empty) => Empty
+    case (_, Cons(_, t)) => t().drop(n - 1)
+  }
+
+  def foldRight[B](z: => B)(f: (A, => B) => B): B =
+    this match {
+      case Cons(h, t) => f(h(), t().foldRight(z)(f))
+      case _ => z
+    }
+
+  def takeWhile(p: A => Boolean): Stream[A] = this match {
+    case Cons(h, t) =>
+      if (p(h())) cons(h(), t().takeWhile(p)) else Empty
+    case Empty => Empty
+  }
+
+  def takeWhileFR(p: A => Boolean): Stream[A] = foldRight(Stream[A]()) {
+    (a, b) => if (p(a)) cons(a, b) else b
+  }
+
+  def forAll(p: A => Boolean): Boolean = {
+    this match {
+      case Cons(h, t) => p(h()) && t().forAll(p)
+      case Empty => true
+    }
+  }
+
+  def headOption: Option[A] = foldRight(None: Option[A])((h, _) => Some(h))
+
+//  def map[B](l: Stream[A])(f: A => B): Stream[B] = ???
+//
+//  def append(l: Stream[A], r: Stream[A]): Stream[A] = ???
+
 
 }
 
--- src/test/scala/org/rurik/part1/part1_5/StreamSpec.scala
+++ src/test/scala/org/rurik/part1/part1_5/StreamSpec.scala
@@ -8,8 +8,48 @@ class StreamSpec extends AnyFlatSpec with should.Matchers {
   "Stream" should "operate toList correctly" in {
 
     Stream[Int](1, 2, 3, 4, 5).toList shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  "Stream" should "operate take correctly" in {
+    Stream[Int]().take(10) shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).take(0) shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).take(3) shouldBe List(1, 2, 3)
+    Stream[Int](1, 2, 3, 4, 5).take(5) shouldBe List(1, 2, 3, 4, 5)
+    Stream[Int](1, 2, 3, 4, 5).take(10) shouldBe List(1, 2, 3, 4, 5)
 
   }
+
+  "Stream" should "operate drop correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).drop(1).toList shouldBe List(2, 3, 4, 5)
+    Stream[Int](1, 2, 3, 4, 5).drop(3).toList shouldBe List(4, 5)
+    Stream[Int](1, 2, 3, 4, 5).drop(5).toList shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).drop(10).toList shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).drop(0).toList shouldBe List(1, 2, 3, 4, 5)
+  }
+
+  "Stream" should "operate takeWhile correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).takeWhile(_ <= 3).toList shouldBe List(1, 2, 3)
+    Stream[Int](1, 2, 3, 4, 5).takeWhile(_ <= 0).toList shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).takeWhile(_ <= 10).toList shouldBe List(1, 2, 3, 4, 5)
+    Stream[Int](1, 2, 3, 4, 5).takeWhile(_ <= -10).toList shouldBe List()
+
+
+    Stream[Int](1, 2, 3, 4, 5).takeWhileFR(_ <= 3).toList shouldBe List(1, 2, 3)
+    Stream[Int](1, 2, 3, 4, 5).takeWhileFR(_ <= 0).toList shouldBe List()
+    Stream[Int](1, 2, 3, 4, 5).takeWhileFR(_ <= 10).toList shouldBe List(1, 2, 3, 4, 5)
+    Stream[Int](1, 2, 3, 4, 5).takeWhileFR(_ <= -10).toList shouldBe List()
+  }
+
+  "Stream" should "operate forAll correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).forAll(_ <= 3) shouldBe false
+    Stream[Int](1, 2, 3, 4, 5).forAll(_ <= 6) shouldBe true
+  }
+
+  "Stream" should "operate headOption correctly" in {
+    Stream[Int](1, 2, 3, 4, 5).headOption shouldBe Some(1)
+    Stream[Int]().headOption shouldBe None
+  }
+
 
 
 }
