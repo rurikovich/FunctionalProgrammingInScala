55e0f1be318a7686406d2d8b8715bfadd241007f
rurik <urrast@yandex.ru>
2020-11-17 01:08:55 +0400

    listOfN

--- src/main/scala/org/rurik/part2/chapter8/Gen.scala
+++ src/main/scala/org/rurik/part2/chapter8/Gen.scala
@@ -1,5 +1,6 @@
 package org.rurik.part2.chapter8
 
+import org.rurik.part1.part1_6.RNG.Rand
 import org.rurik.part1.part1_6.{RNG, State}
 
 case class Gen[A](sample: State[RNG, A])
@@ -10,4 +11,19 @@ object Gen {
     Gen[Int](State(RNG.nonNegativeBetween(start, stopExclusive)(_)))
   }
 
+  def unit[A](a: => A): Gen[A] = Gen[A](State(RNG.unit(a)(_)))
+
+  def boolean: Gen[Boolean] = Gen[Boolean](State(RNG.map(RNG.int)(_ > 0)))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = Gen[List[A]](
+    State(
+      (rnd: RNG) => {
+        val randList: List[Rand[A]] = (0 until n).map {
+          _ => (r: RNG) => g.sample.run(r)
+        }.toList
+
+        RNG.sequence(randList)(rnd)
+      }
+    )
+  )
 }
--- src/test/scala/org/rurik/part2/chapter8/GenSpec.scala
+++ src/test/scala/org/rurik/part2/chapter8/GenSpec.scala
@@ -1,6 +1,6 @@
 package org.rurik.part2.chapter8
 
-import org.rurik.part1.part1_6.SimpleRNG
+import org.rurik.part1.part1_6.{RNG, SimpleRNG, State}
 import org.rurik.part2.chapter8.Gen.choose
 import org.scalacheck
 import org.scalacheck.Prop.forAll
@@ -10,9 +10,9 @@ import org.scalatestplus.scalacheck.Checkers
 
 class GenSpec extends AnyFlatSpec with Checkers with should.Matchers {
 
-  "choose" should "gen new values between start and end correctly" in {
-    val rnd = SimpleRNG(1)
+  val rnd = SimpleRNG(1)
 
+  "choose" should "gen new values between start and end correctly" in {
     val startGen = org.scalacheck.Gen.choose(0, 100)
     val endGen = org.scalacheck.Gen.choose(101, 200)
 
@@ -23,8 +23,26 @@ class GenSpec extends AnyFlatSpec with Checkers with should.Matchers {
           v > start && v < end
       }
     }
-
-
   }
+
+
+  val intGen: Gen[Int] = Gen(State(_.nextInt))
+
+  "listOfN" should "gen list of size n correctly" in {
+    val nStart = 5
+    val nEnd = 100
+    val nGen = org.scalacheck.Gen.choose(nStart, nEnd)
+
+    check {
+      forAll(nGen) {
+        n: Int =>
+          val listGen: Gen[List[Int]] = Gen.listOfN(n, intGen)
+          val list: List[Int] = listGen.sample.run(rnd)._1
+
+          list.length == n
+      }
+    }
+  }
+
 
 }
