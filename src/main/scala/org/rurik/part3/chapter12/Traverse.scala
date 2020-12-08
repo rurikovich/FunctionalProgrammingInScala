6325e050b31391a2ef299f0c4c5191278b3792ba
rurik <urrast@yandex.ru>
2020-12-08 20:07:04 +0400

    treeTraverse

--- src/main/scala/org/rurik/part3/chapter12/Traverse.scala
+++ src/main/scala/org/rurik/part3/chapter12/Traverse.scala
@@ -43,13 +43,25 @@ object Traverse {
     override def foldRight[A, B](as: Tree[A])(z: B)(f: (A, B) => B): B =
       (as.head, as.tail) match {
         case (h, Nil) => f(h, z)
-        case (h, t) => f( t.foldRight(f(h, z))() ,f(h, z))
+        case (h: A, t: List[Tree[A]]) =>
+          t.foldRight(f(h, z)) {
+            (tree: Tree[A], acc: B) =>
+              foldRight(tree)(acc)(f)
+          }
       }
 
 
-    override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = ???
+    override def foldLeft[A, B](as: Tree[A])(z: B)(f: (B, A) => B): B = (as.head, as.tail) match {
+      case (h, Nil) => f(z, h)
+      case (h: A, t: List[Tree[A]]) =>
+        t.foldLeft(f(z, h)) {
+          (acc: B, tree: Tree[A]) =>
+            foldLeft(tree)(acc)(f)
+        }
+    }
 
-    override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B = ???
+    override def foldMap[A, B](as: Tree[A])(f: A => B)(mb: Monoid[B]): B =
+      foldRight(as)(mb.zero)((a, b) => mb.op(f(a), b))
 
     override def map[A, B](fa: Tree[A])(f: A => B): Tree[B] =
       fa.copy(f(fa.head), fa.tail.map(t => map(t)(f)))
