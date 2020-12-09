fff611ffd37932988e57803adc3f0f571a4329e1
rurik <urrast@yandex.ru>
2020-12-09 18:18:54 +0400

    def map[A,B](fa: F[A])(f: A => B): F[B] =     traverse[Id, A, B](fa)(f)(idMonad)

--- src/main/scala/org/rurik/part3/chapter12/Traverse.scala
+++ src/main/scala/org/rurik/part3/chapter12/Traverse.scala
@@ -8,17 +8,16 @@ trait Traverse[F[_]] extends Functor[F] with Foldable[F] {
   def traverse[G[_] : Applicative, A, B](fa: F[A])(f: A => G[B]): G[F[B]]
 
   def sequence[G[_] : Applicative, A](fga: F[G[A]]): G[F[A]] =
-    traverse(fga)(ga => ga)
+    traverse(fga)((ga: G[A]) => ga)
 
   type Id[A] = A
 
   val idMonad = new Monad[Id] {
     def unit[A](a: => A) = a
-
-    override def flatMap[A, B](a: A)(f: A => B): B = f(a)
+    override def flatMap[A,B](a: A)(f: A => B): B = f(a)
   }
 
-  def map[A, B](fa: F[A])(f: A => B): F[B] =
+  def map[A,B](fa: F[A])(f: A => B): F[B] =
     traverse[Id, A, B](fa)(f)(idMonad)
 
   import Applicative._
