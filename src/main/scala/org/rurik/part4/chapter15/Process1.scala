5350e9bb9b4fcdefa62330375b3eddb99db43e11
rurik <urrast@yandex.ru>
2020-12-24 14:59:57 +0400

    fix

--- src/main/scala/org/rurik/part4/chapter15/Process1.scala
+++ src/main/scala/org/rurik/part4/chapter15/Process1.scala
@@ -10,22 +10,22 @@ import scala.collection.immutable.LazyList.#::
 sealed trait Process1[I, O] {
 
   def apply(s: LazyList[I]): LazyList[O] = this match {
-    case Halt() => LazyList()
-    case Await(recv) => s match {
+    case Halt1() => LazyList()
+    case Await1(recv) => s match {
       case h #:: t => recv(Some(h))(t)
       case xs => recv(None)(xs)
     }
-    case Emit(h, t) => h #:: t(s)
+    case Emit1(h, t) => h #:: t(s)
   }
 
   def repeat: Process1[I, O] = {
     def go(p: Process1[I, O]): Process1[I, O] = p match {
-      case Halt() => go(this)
-      case Await(recv) => Await {
+      case Halt1() => go(this)
+      case Await1(recv) => Await1 {
         case None => recv(None)
         case i => go(recv(i))
       }
-      case Emit(h, t) => Emit(h, go(t))
+      case Emit1(h, t) => Emit1(h, go(t))
     }
 
     go(this)
@@ -33,12 +33,12 @@ sealed trait Process1[I, O] {
 
   def |>[O2](p2: Process1[O, O2]): Process1[I, O2] = {
     p2 match {
-      case Halt() => Halt()
-      case Emit(h: O2, t: Process1[I, O]) => Emit(h, this |> t)
-      case Await(f) => this match {
-        case Emit(h, t) => t |> f(Some(h))
-        case Halt() => Halt[I, O]() |> f(None)
-        case Await(g) => Await((i: Option[I]) => g(i) |> p2)
+      case Halt1() => Halt1()
+      case Emit1(h: O2, t: Process1[I, O]) => Emit1(h, this |> t)
+      case Await1(f) => this match {
+        case Emit1(h, t) => t |> f(Some(h))
+        case Halt1() => Halt1[I, O]() |> f(None)
+        case Await1(g) => Await1((i: Option[I]) => g(i) |> p2)
       }
     }
   }
@@ -46,26 +46,26 @@ sealed trait Process1[I, O] {
   def map[O2](f: O => O2): Process1[I, O2] = this |> lift(f)
 
   def ++(p: => Process1[I, O]): Process1[I, O] = this match {
-    case Halt() => p
-    case Emit(h, t) => Emit(h, t ++ p)
-    case Await(recv) => Await(recv andThen (_ ++ p))
+    case Halt1() => p
+    case Emit1(h, t) => Emit1(h, t ++ p)
+    case Await1(recv) => Await1(recv andThen (_ ++ p))
   }
 
   def flatMap[O2](f: O => Process1[I, O2]): Process1[I, O2] = this match {
-    case Halt() => Halt()
-    case Emit(h, t) => f(h) ++ t.flatMap(f)
-    case Await(recv) => Await(recv andThen (_ flatMap f))
+    case Halt1() => Halt1()
+    case Emit1(h, t) => f(h) ++ t.flatMap(f)
+    case Await1(recv) => Await1(recv andThen (_ flatMap f))
   }
 
 
 }
 
 
-case class Emit[I, O](head: O, tail: Process1[I, O] = Halt[I, O]()) extends Process1[I, O]
+case class Emit1[I, O](head: O, tail: Process1[I, O] = Halt1[I, O]()) extends Process1[I, O]
 
-case class Await[I, O](recv: Option[I] => Process1[I, O]) extends Process1[I, O]
+case class Await1[I, O](recv: Option[I] => Process1[I, O]) extends Process1[I, O]
 
-case class Halt[I, O]() extends Process1[I, O]
+case class Halt1[I, O]() extends Process1[I, O]
 
 
 object Process1 {
@@ -77,30 +77,30 @@ object Process1 {
     lift(f) |> any
 
   def liftOne[I, O](f: I => O): Process1[I, O] =
-    Await {
-      case Some(i) => Emit(f(i))
-      case None => Halt()
+    Await1 {
+      case Some(i) => Emit1(f(i))
+      case None => Halt1()
     }
 
   def lift[I, O](f: I => O): Process1[I, O] = liftOne(f).repeat
 
   def await[I, O](f: I => Process1[I, O],
-                  fallback: Process1[I, O] = Halt[I, O]()): Process1[I, O] =
-    Await[I, O] {
+                  fallback: Process1[I, O] = Halt1[I, O]()): Process1[I, O] =
+    Await1[I, O] {
       case Some(i) => f(i)
       case None => fallback
     }
 
   def filter[I](p: I => Boolean): Process1[I, I] =
-    Await[I, I] {
-      case Some(i) if p(i) => Emit(i)
-      case _ => Halt()
+    Await1[I, I] {
+      case Some(i) if p(i) => Emit1(i)
+      case _ => Halt1()
     }.repeat
 
 
   def take[I](n: Int): Process1[I, I] = n match {
-    case 0 => Halt()
-    case n => await(i => Emit[I, I](i, take(n - 1)))
+    case 0 => Halt1()
+    case n => await(i => Emit1[I, I](i, take(n - 1)))
   }
 
   /* The identity `Process`, just repeatedly echos its input. */
@@ -108,24 +108,24 @@ object Process1 {
 
   def drop[I](n: Int): Process1[I, I] = n match {
     case i if i <= 0 => id
-    case _ => Await(_ => drop(n - 1))
+    case _ => Await1(_ => drop(n - 1))
 
   }
 
   def takeWhile[I](f: I => Boolean): Process1[I, I] = await {
     i =>
-      if (f(i)) Emit(i, takeWhile(f)) else Halt()
+      if (f(i)) Emit1(i, takeWhile(f)) else Halt1()
   }
 
   def dropWhile[I](f: I => Boolean): Process1[I, I] = await {
     i =>
-      if (f(i)) dropWhile(f) else Emit(i, id)
+      if (f(i)) dropWhile(f) else Emit1(i, id)
   }
 
   def count[I]: Process1[I, Int] = {
     def go(n: Int): Process1[I, Int] = {
       await {
-        _ => Emit(n + 1, go(n + 1))
+        _ => Emit1(n + 1, go(n + 1))
       }
     }
 
@@ -136,7 +136,7 @@ object Process1 {
   def mean: Process1[Double, Double] = {
     def go(n: Int, sum: Double): Process1[Double, Double] = {
       await {
-        i => Emit(sum / n + 1, go(n + 1, sum + i))
+        i => Emit1(sum / n + 1, go(n + 1, sum + i))
       }
     }
 
@@ -147,7 +147,7 @@ object Process1 {
     await(
       (i: I) =>
         f(i, z) match {
-          case (o, s2) => Emit(o, loop(s2)(f))
+          case (o, s2) => Emit1(o, loop(s2)(f))
         }
     )
 
@@ -157,15 +157,15 @@ object Process1 {
     @annotation.tailrec
     def go(ss: Iterator[String], cur: Process1[String, A], acc: B): B =
       cur match {
-        case Halt() => acc
-        case Await(recv) =>
+        case Halt1() => acc
+        case Await1(recv) =>
           val next = if (ss.hasNext)
             recv(Some(ss.next))
           else
             recv(None)
           go(ss, next, acc)
 
-        case Emit(h, t) =>
+        case Emit1(h, t) =>
           go(ss, t, g(acc, h))
       }
 
